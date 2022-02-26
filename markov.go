c97771f2b55489d3854aa66c71abc68f1732df3b
Patrick Groh <contact@patrickgroh.com>
2022-02-26 01:37:00 -0600

    Maybe massively improved performance of building huge graphs

--- markov.go
+++ markov.go
@@ -6,24 +6,30 @@ import (
 )
 
 //PUBLIC
+type Graph interface {
+	LoadPhrase(string)
+	GenerateMarkovString() string
+}
 
 //Graph is the generic holder of all of the data for a certain graph of words needed to generate a markov chain.
-type Graph struct {
+type RamGraph struct {
 	starters     list
 	allWords     list
 	strings      []string
 	contextDepth int
+	nodes        map[string]*link
 }
 
-func NewGraph(depth int) *Graph {
-	var g Graph
+func NewGraph(depth int) *RamGraph {
+	var g RamGraph
+	g.nodes = make(map[string]*link)
 	g.contextDepth = depth
 	return &g
 }
 
 //LoadPhrase loads a whole phrase (be it a sentence, single word that someone said, or paragraph) into the graph.
 //Splits the phrase along spaces in order to load each word into the chain.
-func (graph *Graph) LoadPhrase(phrase string) {
+func (graph *RamGraph) LoadPhrase(phrase string) {
 	words := strings.Split(phrase, " ")
 	// Get the words in reverse order, just to make it easier to input the next word in the chain.
 	var prevVal *link = nil //Start prevVal as nil, as the first word to appear will be the last one in the phrase.
@@ -39,7 +45,7 @@ func (graph *Graph) LoadPhrase(phrase string) {
 }
 
 //GenerateMarkovString generates a random string based off of the current model in the graph.
-func (graph *Graph) GenerateMarkovString() string {
+func (graph *RamGraph) GenerateMarkovString() string {
 	var ret string
 	var currLink *link = graph.starters.links[rand.Intn(len(graph.starters.links))]
 	ret += *currLink.value
@@ -65,8 +71,9 @@ type list struct {
 
 //loadWord loads a word into the respective graph. It creates a link if one doesn't exist,
 // and links the current word to both previous (if higher context is on) and next words.
-func (graph *Graph) loadWord(val string, nextval *link, starter bool, context []*string) *link {
-	l := graph.findInGraph(val, context)
+func (graph *RamGraph) loadWord(val string, nextval *link, starter bool, context []*string) *link {
+	mapKey := buildMapKey(val, context)
+	l := graph.nodes[mapKey]
 	if l == nil {
 		ctx := make([]*string, len(context))
 		for i, v := range context {
@@ -78,6 +85,7 @@ func (graph *Graph) loadWord(val string, nextval *link, starter bool, context []
 			context: ctx,
 		}
 		graph.allWords.links = append(graph.allWords.links, l)
+		graph.nodes[mapKey] = l
 	} else {
 		l.links = append(l.links, nextval)
 	}
@@ -88,27 +96,35 @@ func (graph *Graph) loadWord(val string, nextval *link, starter bool, context []
 	return l
 }
 
-func (graph *Graph) findInGraph(val string, context []*string) *link {
-	for i, v := range graph.allWords.links {
-		if *v.value == val {
-			contextMatching := true
-			if len(v.context) != len(context) {
-				continue
-			}
-			for j, ctx := range v.context {
-				if *ctx != *context[j] {
-					contextMatching = false
-				}
-			}
-			if contextMatching {
-				return graph.allWords.links[i]
-			}
-		}
+func (graph *RamGraph) findInGraph(val string, context []*string) *link {
+	key := ""
+	for _, ctx := range context {
+		key += *ctx + " "
 	}
-	return nil
+	key += val
+	return graph.nodes[key]
+
+	// for i, v := range graph.allWords.links {
+	// 	if *v.value == val {
+	// 		contextMatching := true
+	// 		if len(v.context) != len(context) {
+	// 			continue
+	// 		}
+	// 		for j, ctx := range v.context {
+	// 			if *ctx != *context[j] {
+	// 				contextMatching = false
+	// 			}
+	// 		}
+	// 		if contextMatching {
+	// 			return graph.allWords.links[i]
+	// 		}
+	// 	}
+	// }
+	// return nil
 }
 
-func (graph *Graph) findString(val string) *string {
+func (graph *RamGraph) findString(val string) *string {
+
 	for i, v := range graph.strings {
 		if v == val {
 			return &graph.strings[i]
@@ -123,4 +139,13 @@ func lesser(x int, y int) int {
 		return y
 	}
 	return x
+}
+
+func buildMapKey(val string, context []*string) string {
+	key := ""
+	for _, ctx := range context {
+		key += *ctx + " "
+	}
+	key += val
+	return key
 }
